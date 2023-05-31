e44836f4cab997d74b1ea120b0d233a0ec0cc0c1
Avel Docquin <adocquin@outlook.com>
2023-06-01 01:22:14 +0200

    Boxplot update & added start of stats module.

--- coolpandas/plot/boxplot.py
+++ coolpandas/plot/boxplot.py
@@ -12,6 +12,7 @@ def boxplot(
     y_axis: str,
     title: str,
     subtitle: str | None = None,
+    outliers: bool = True,
     **kwargs,
 ) -> go.Figure:
     """Create a box plot.
@@ -22,24 +23,35 @@ def boxplot(
         y_axis (str): Column to use as y axis.
         title (str): Title of the plot.
         subtitle (str, optional): Subtitle of the plot. Defaults to None.
-        **kwargs: Keyword arguments to pass to plotly.express.box.
+        outliers (bool, optional): Show outliers. Defaults to True.
+        **kwargs: Keyword arguments to pass to plotly.graph_objects.Box.
 
     Returns:
         go.Figure: Box plot figure.
     """
-    if "color" not in kwargs:
-        kwargs["color"] = x_axis
-    fig = px.box(
-        data_frame,
-        x=x_axis,
-        y=y_axis,
-        notched=True,
+    fig = go.Figure()
+    for x_value in data_frame[x_axis].unique():
+        y_values = data_frame[data_frame[x_axis] == x_value][y_axis]
+
+        boxpoints = "outliers" if outliers else False
+
+        fig.add_trace(
+            go.Box(
+                x=data_frame[data_frame[x_axis] == x_value][x_axis],
+                y=y_values,
+                name=str(x_value),
+                boxpoints=boxpoints,
+                **kwargs,
+            )
+        )
+
+    fig.update_layout(
         title=format_title(title, subtitle=subtitle),
         template=custom_template,
         width=800,
         height=400,
-        **kwargs,
+        showlegend=False,
     )
-    if kwargs.get("color") == x_axis:
-        fig.layout.update(showlegend=False)
+    fig.update_xaxes(title_text=x_axis)
+    fig.update_yaxes(title_text=y_axis)
     return fig
